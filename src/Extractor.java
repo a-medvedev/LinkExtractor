eb5b30fdfc7943581198c749e537bcc27eab18ad
tantal <ib.medvedev@rambler.ru>
2013-10-25 14:50:17 +0700

    25.10 14:49

--- src/Extractor.java
+++ src/Extractor.java
@@ -1,2 +1,23 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Extractor {
+    private URL current;
+    private List<String> extracted_links = new ArrayList<String>();
+
+
+    Extractor(String url) throws MalformedURLException {
+        if (url.isEmpty()){
+            throw new MalformedURLException();
+        } else {
+            current = new URL(url);
+            extracted_links.add(current.toString());
+        }
+    }
+
+    public List<String> extract(List<String> links){
+        List<String>
+    }
 }
---
+++ src/Launcher.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: tantal
+ * Date: 25.10.13
+ * Time: 13:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class Launcher {
+    public static void main(String[] args){
+
+    }
+}
