e7cd37f999e41a5bb421729a821462310bf354e5
tantal <ib.medvedev@rambler.ru>
2013-10-30 11:12:42 +0700

    30.10 11:12

--- src/Extractor.java
+++ src/Extractor.java
@@ -1,25 +1,69 @@
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Extractor {
-    private URL current;
-    private List<String> extracted_links = new ArrayList<String>();
 
+    public List<URL> extract(URL root, int depth){
+        List<URL> current = new ArrayList<>(), result = new ArrayList<>();
+        List<String> extracted = new ArrayList<>();
 
-    Extractor(String url) throws MalformedURLException {
-        if (url.isEmpty()){
-            throw new MalformedURLException();
-        } else {
-            current = new URL(url);
-            extracted_links.add(current.toString());
+        current.add(root);
+        for (int i = 1; i == depth; i++){
+            System.out.println("Глубина " + i);
+
+            //перебираем каждый URL, экстрагируем ссылки, добавляем их в общую копилку
+            for(URL u : current){
+                try {
+                    URLConnection conn = u.openConnection();
+                    //получаем тип содержимого по открытому соединению
+                    String type = conn.getContentType();
+                    //нас интересует только гипертекстовое содержимое
+                    if(type.toLowerCase().contains("text") && type.toLowerCase().contains("html")){
+                        Scanner input = new Scanner(conn.getInputStream()); //для построчного чтения страницы
+                        StringBuilder contentBuilder = new StringBuilder(); //для соединения всех строк в одну
+
+                        //Склеиваем страницу, пока есть из чего
+                        while (input.hasNextLine()){
+                            contentBuilder.append(input.nextLine());
+                        }
+                        String content = contentBuilder.toString(); //Страмица целиком
+
+                    }
+                } catch (IOException e) {
+                    //Соединение не открыто
+                }
+            }
         }
     }
 
-    public List<String> extract(URL link){
-        List<String> result = new ArrayList<String>();
+    private List<URL> convertToURLList(List<String> links){
+        if (links.isEmpty()){
+            System.out.println("Нечего конвертировать. Список пуст.");
+            return null;
+        }
 
+        List<URL> result = new ArrayList<URL>();
+
+        //counter - общее кол-во ссылок, passed -  удачно сконвертированные ссылки, failed - несконвертированные ссылки
+        int counter = 0, passed = 0, failed = 0;
+        for(String s : links){
+            counter ++;
+            URL u;
+            try {
+                u = new URL(s);
+                result.add(u);
+                passed++;
+            } catch (MalformedURLException e) {
+                System.out.println("Невозможно преобразовать: " + s);
+                failed++;
+            }
+        }
+        System.out.println("Всего: " + counter + "Удачно: " + passed + "Неудачно: " + failed);
         return result;
     }
 }
