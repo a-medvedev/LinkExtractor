369a6d2d3ce0006d3b7ca4427c46ed03097feca4
tantal <ib.medvedev@rambler.ru>
2013-11-01 09:05:15 +0700

    1.11 9:04

--- src/Probe.java
+++ src/Probe.java
@@ -1,34 +1,49 @@
 import java.io.*;
-import java.util.Scanner;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Probe {
     public static void main(String[] args) throws IOException {
+        URL url = new URL("http://kstatida.ru");
+        URLConnection conn = url.openConnection();
+        InputStream is = conn.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-//        System.out.print("Input filename: ");
-//        Scanner input = new Scanner(System.in);
-        //String str = input.nextLine();
+        String s;
+        StringBuilder sb = new StringBuilder();
+        while ((s = reader.readLine()) != null){
+            sb.append(s);
+        }
 
-       // File f = new File(input.nextLine());
+        Pattern p = Pattern.compile("(href=\")([\\p{Graph}]*)(\")");
+        Matcher m = p.matcher(sb.toString());
+        while(m.find()){
+            //Разбивать по знаку вопроса, чтобы отделить параметры ссылки от самой ссылки.
+            //Добавить алгоритмы распознавания файлов, отсев одинаковых ссылок
+            System.out.println(m.group(2).split("[?]")[0]);
+
+        }
+    }
+
+    private void fromFile() throws IOException {
         File f = new File("/home/tantal/bash.html");
-        //Scanner s = new Scanner(f);
 
         BufferedReader reader = new BufferedReader(new FileReader(f));
         String s;
         StringBuilder sb = new StringBuilder();
         while ((s = reader.readLine()) != null){
-            //System.out.println(reader);
             sb.append(s);
         }
-        //System.out.println(sb.toString());
 
-        Pattern p = Pattern.compile("(href=\")([\\w|_|\\|/]*)(\")");
+        Pattern p = Pattern.compile("(href=\")([\\p{Graph}]*)(\")");
         Matcher m = p.matcher(sb.toString());
         while(m.find()){
-            //System.out.println(sb.toString());
+            //Разбивать по знаку вопроса, чтобы отделить параметры ссылки от самой ссылки.
+            //Добавить алгоритмы распознавания файлов, отсев одинаковых ссылок
+            System.out.println(m.group(2).split("[?]")[0]);
 
-            System.out.println(m.group(2));
         }
     }
 }
